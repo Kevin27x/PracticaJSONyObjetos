4cbc29e8c58c34ef12b2d377fce344b792c1069a
kevincollao <kevin.collao1@gmail.com>
2022-01-27 18:05:48 -0300

    Llamando a API, con ASYN, AWAIT, .JSON

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var persona = {
     nombre: "kevin",
     profesion: "desarrollador"
@@ -5,3 +6,23 @@ var persona = {
 let estringefiar = JSON.stringify(persona);
 console.log(estringefiar)
 console.log(typeof(estringefiar))
+*/
+async function obtenerDatos(){
+    const response = await fetch("http://127.0.0.1:5500/Practica/JSONYOBJETOS/primer.json");
+    
+    //const json = await response.text();
+    // el .json del final, hace lo mismo que JSON.parse(json)
+    const json = await response.json();
+    
+    //console.log(JSON.parse(json))
+    console.log(json)
+    console.log(json.nombre)
+    console.log(json.conocimientos)
+    console.log(json.conocimientos[0])
+    console.log(json.direccion.comuna)
+    json.arregloDeObjetosExperiencia.forEach((element) => {
+        console.log(element.empresa);
+        console.log(element.anios);
+    });
+}   
+obtenerDatos()
